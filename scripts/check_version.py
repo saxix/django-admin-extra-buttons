fb4d348f5baa59cdbab5fa2e52c37ded0f0bd80b
Stefano Apostolico <s.apostolico@gmail.com>
2024-10-10 09:31:26 +0200

    minor improvements to tox.ini

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -6,7 +6,9 @@ from importlib.metadata import version
 import toml
 from admin_extra_buttons import VERSION
 
-expected = toml.load(Path('pyproject.toml').open())['project']['version']
+expected = toml.load((Path(__file__).parent.parent / 'pyproject.toml').open())['project']['version']
+print("Expected version:", expected)
+print("Metadata version:", version('django_admin_extra_buttons'))
+print("Package version:", VERSION)
 assert VERSION == expected
 assert version('django_admin_extra_buttons') == expected
-print(VERSION)
