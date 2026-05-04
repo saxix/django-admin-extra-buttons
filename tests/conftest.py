93f8ef455c18198b49cf8c26ce10161760899210
David Guillot <david.guillot@beta.gouv.fr>
2026-05-04 16:40:16 +0200

    tests: don't try to cover for Python/Django incompatibilities
    Django 4.x does not officially support Python 3.14, we should not try to make it work

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,26 +17,6 @@ def pytest_configure(config):
 
     import django
 
-    # Python 3.14 and Django 4.2 have a conflict in Context.__copy__
-    # triggered by instrumented_test_render.
-    if sys.version_info >= (3, 14) and django.VERSION < (5, 0):
-        from django.template import context
-
-        def __copy__(self):
-            # We must return a new instance that behaves like a Context
-            # and contains the same data, but avoids the broken super().__copy__()
-            cls = self.__class__
-            result = cls.__new__(cls)
-            # Copy dictionaries
-            result.dicts = self.dicts[:]
-            # Copy other attributes that might be needed by Django
-            for attr in ("render_context", "template", "autoescape", "use_l10n", "use_tz"):
-                if hasattr(self, attr):
-                    setattr(result, attr, getattr(self, attr))
-            return result
-
-        context.BaseContext.__copy__ = __copy__
-
     django.setup()
 
 
